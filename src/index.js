0121f17871ad09b8c66766dd216743b51c8a68ab
James Dean <kreebog@gmail.com>
2022-08-22 14:48:55 -0400

    react tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+// import { createRoot } from 'react-dom/client'
+import * as ReactDOMClient from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Square extends React.Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render() {
+    return (
+      <button
+        className='square'
+        onClick={() => {
+            this.props.onClick()
+        }}
+      >
+        {this.props.value}
+      </button>
+    )
+  }
+}
+
+class Board extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state  = {
+      squares: Array(9).fill(null)
+    }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({squares: squares})
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
+  }
+
+  render() {
+    const status = 'Next Player: X'
+
+    return (
+      <div>
+        <div className='status>'>{status}</div>
+        <div className='board-row'>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className='board-row'>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className='game'>
+        <div className='game-baord'>
+          <Board />
+        </div>
+        <div className='game-info'>
+          <div>{/* status */}</div>
+          <div>{/* TODO */}</div>
+        </div>
+      </div>
+    )
+  }
+}
+
+const root = ReactDOMClient.createRoot(document.getElementById('root'))
+root.render(<Game />)
